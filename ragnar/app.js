e9213b39aa4cb22f07df530029d82f365f16fcc9
Phillip Heyrman <phillip.heyrman@gmail.com>
2014-04-09 03:40:31 +0000

    Added bootstrap and angular from bower.

--- ragnar/app.js
+++ ragnar/app.js
@@ -21,8 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public2', express.static(path.join(__dirname, 'bower_components')));
 
 // development only
 if ('development' == app.get('env')) {
